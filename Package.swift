f1796b6589224ac9d92303b6ded1d8ad04a9f9ef
ueshun109 <ush109rock@gmail.com>
2022-02-01 21:01:59 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,25 +4,55 @@
 import PackageDescription
 
 let package = Package(
-    name: "GoolgeMaps-SPM",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "GoolgeMaps-SPM",
-            targets: ["GoolgeMaps-SPM"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "GoolgeMaps-SPM",
-            dependencies: []),
-        .testTarget(
-            name: "GoolgeMaps-SPMTests",
-            dependencies: ["GoolgeMaps-SPM"]),
-    ]
+  name: "GoolgeMaps-SPM",
+  products: [
+    .library(
+      name: "GoogleMaps",
+      targets: ["GoogleMaps"]
+    ),
+    .library(
+      name: "GoogleMapsBase",
+      targets: ["GoogleMapsBase"]
+    ),
+    .library(
+      name: "GoogleMapsCore",
+      targets: ["GoogleMapsCore"]
+    ),
+    .library(
+      name: "GoogleMapsM4B",
+      targets: ["GoogleMapsM4B"]
+    ),
+    .library(
+      name: "GooglePlaces",
+      targets: ["GooglePlaces"]
+    ),
+  ],
+  dependencies: [],
+  targets: [
+    .binaryTarget(
+      name: "GoogleMaps",
+      url: "https://github.com/ueshun109/GoogleMaps-SPM/releases/download/6.0.1/GoogleMaps.xcframework.zip",
+      checksum: "3ebf773e0709f8c834d5d654932604902cb564862146dbf087e11372519623fe"
+    ),
+    .binaryTarget(
+      name: "GoogleMapsBase",
+      url: "https://github.com/ueshun109/GoogleMaps-SPM/releases/download/6.0.1/GoogleMapsBase.xcframework.zip",
+      checksum: "792100bdd0a148b52417aa95c8b0cd97bdf73b0a16ab767153ab34813fa3aaae"
+    ),
+    .binaryTarget(
+      name: "GoogleMapsCore",
+      url: "https://github.com/ueshun109/GoogleMaps-SPM/releases/download/6.0.1/GoogleMapsCore.xcframework.zip",
+      checksum: "c67ac0d66f38b551956a0ce9c16ada0e372d46ca91bbc679af76e5ee9abe884c"
+    ),
+    .binaryTarget(
+      name: "GoogleMapsM4B",
+      url: "https://github.com/ueshun109/GoogleMaps-SPM/releases/download/6.0.1/GoogleMapsM4B.xcframework.zip",
+      checksum: "9d0701950d3036ba9b5a567386df3c2f0378459b7f7002e48b9b4b5fbcfd48e7"
+    ),
+    .binaryTarget(
+      name: "GooglePlaces",
+      url: "https://github.com/ueshun109/GoogleMaps-SPM/releases/download/6.0.1/GooglePlaces.xcframework.zip",
+      checksum: "4e0b92b6f26e7320351e13be58abeea030f015827382d129022ba3c47cd227c9"
+    ),
+  ]
 )
